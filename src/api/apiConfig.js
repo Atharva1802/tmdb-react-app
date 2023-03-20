096e752cf5fc567a1de2ec809705cd63a3fd742c
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-20 20:40:20 +0530

    Api configured

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = { 
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: 'b39a02f9d49ce797ce8f43804d3a68e1',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig; 
