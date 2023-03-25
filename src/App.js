eb63194633ec873ba4013b596263d9602c538c40
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-25 12:28:17 +0530

    Box icon s

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-
+import 'swiper/swiper.min.css'
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './App.scss';
+
+import { BrowserRouter } from 'react-router-dom'
+
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+import Routes from './config/Routes';
+
 
 function App() {
   return (
