41b8b24fdf6181128d403ba2b66f87a11e982d43
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-30 11:03:15 +0530

    Header fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import 'swiper/swiper.min.css'
-import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom';
 
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 import Routes from './config/Router';
 
-
 function App() {
-  return (
-    <div>
-      App
-      <Header />
-      
-    </div>
-  );
+    return (
+      <BrowserRouter>
+            <>
+                <Header />
+                <Routes />
+                <Footer />
+            </>
+        </BrowserRouter>
+    );
 }
 
 export default App;
+
