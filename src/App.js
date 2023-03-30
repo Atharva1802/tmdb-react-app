f79fe6a3153da0ccdae15cd402e48cc03e71aa0e
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-03-30 11:09:21 +0530

    Minor scss changes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       <BrowserRouter>
             <>
                 <Header />
-                <Routes />
-                <Footer />
+                {/* <Routes /> */}
+                {/* <Footer /> */}
             </>
         </BrowserRouter>
     );
