39d532aaf025838ed9337d2039fdb1ab3cd51096
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-04-01 10:53:52 +0530

    Wrapper Error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react"
 import HeroSlide from '../components/Hero-Slide/HeroSlide'
+import { Link } from "react-router-dom"
+import { OutlineButton } from "../components/Buttons/Button"
+// import { category } from "../api/tmdbApi"
+// import { movieType } from "../api/tmdbApi"
 
 const Home = () => {
   return (
     <>
-      <HeroSlide />
+            <HeroSlide/>
+            <div className="container">
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Trending Movies</h2>
+                        <Link to="/movie">
+                            <OutlineButton className="small">View more</OutlineButton>
+                        </Link>
+                    </div>
+
+                </div>
+          </div>
     </>
   )
 }
